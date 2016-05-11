53860ff5ee226ed153a90258e848bffcd99ec58b
Sheldon Lynn <sheldonlynn@outlook.com>
2016-05-11 09:54:19 -0700

    part collision things

--- js/game.js
+++ js/game.js
@@ -67,10 +67,16 @@ function Drawable() {
   this.speed = 0;
   this.canvasWidth = 0;
   this.canvasHeight = 0;
+  this.canCollide = "";
+  this.isColliding = false;
+  this.type = "";
 
   // Define abstract function to be implemented in child objects
   this.draw = function () {};
   this.move = function () {};
+  this.canCollideWith = function(object) {
+    return (this.canCollide === object.type);
+  };
 }
 
 /**
