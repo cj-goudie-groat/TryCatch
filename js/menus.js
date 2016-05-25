ff300ad99c99547d287b54a457e0d352de16985e
Connor Goudie <connorgoudie@live.com>
2016-05-25 10:03:10 -0700

    Tutorial button drop down

--- js/menus.js
+++ js/menus.js
@@ -19,7 +19,16 @@ $(document).ready(function () {
    */
   $(".main-menu-button").click(function () {
     $("#leaderboard").css({display: "none"});
+    $("#tutorial-screen").css({display: "none"});
     $("#pause-menu-screen-darken").css({display: "none"});
+  });
+  
+  /**
+   * Brings the tutorial screen up.
+   */
+  $(".tutorial-button").click(function() {
+    $("#tutorial-screen").css({display: "block"});
+    $("#pause-menu-screen-darken").fadeIn(1000);
   });
   
   /**
